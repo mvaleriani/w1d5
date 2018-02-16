8818dc3303ba97102605413f3715af247a66b5b7
Michael Valeriani <michaelvaleriani@gmail.com>
2018-02-16 14:26:29 -0800

    Create add_child method, create remove_child method, fix parent= method

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,26 +1,41 @@
+require "byebug"
+
 class PolyTreeNode
-  #attr_reader :parent, :children, :value
+  attr_reader :parent, :children, :value
 
   def initialize(value)
+    #debugger
     @value = value
     @parent = nil
     @children = []
   end
 
-  def parent
-    self.parent
-  end
+  # def parent
+  #   self.parent
+  # end
 
   def parent=(node)
-    self.parent = node
+
+    if !node.nil?
+      @parent.children.reject! { |child| child == self } unless parent.nil?
+      @parent = node
+      @parent.children << self unless @parent.children.include?(self)
+    else
+      @parent = nil
+    end
+
   end
 
-  def children
-    self.children
+  def add_child(child)
+    @children << child
+    child.parent = self
   end
 
-  def value
-    self.value
+  def remove_child(child)
+    raise "Not a child!" unless @children.include?(child)
+    child.parent = nil
+
+    @children.reject! { |our_child| child == our_child }
   end
 
 end
